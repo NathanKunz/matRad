4f9f814167c971b4ac93b9cea77d382aece7d11a
NathanKunz <nathan9kunz@gmail.com>
2022-02-12 17:50:25 +0100

    remove faulty check
    (added the check to the wrong constrian function in last commit)

--- optimization/@matRad_OptimizationProblem/matRad_constraintJacobian.m
+++ optimization/@matRad_OptimizationProblem/matRad_constraintJacobian.m
@@ -201,10 +201,4 @@ elseif isa(optiProb.BP,'matRad_EffectProjection')
     end
 end
 
-if isgpuarray(c)
-    c = gather(c); % gather c when using gpu operations
-end
-
-c = double(c); % cast if c is single from runing on gpu
-
 end
